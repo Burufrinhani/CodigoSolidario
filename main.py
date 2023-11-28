cfe820d880cb8dd62398f0e2ddda2d08a2b7dc15
bruna <brunitabruninha.bfs@gmail.com>
2023-11-27 23:39:44 -0300

    Refatoração
    Exclusão do arquivo Uteis, e organização da documentação dos pontos de coleta de recursos, para colocá-los no dicionário

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from os import system
+from pontos import pontosDoacoes
 
-# while True:
-print('SOMOS O CÓDIGO SOLIDÁRIO. \n\nAqui você encontra todos os pontos de doação e coleta de suprimentos de São Paulo. \nInsira o número que corresponde ao que deseja consultar: \n1. Postos de coleta \n2. Doações de alimentos \n3. Doações de roupas \n4. Doações de agasalhos \n5. Doções de produtos de higiene')
+def main():
+    print("Escolha o recurso que deseja consultar:")
+    print("1. Comida")
+    print("2. Roupa")
+    print("3. Agasalho")
+    print("4. Higiene")
+    print("4. Doação de sangue")
--- pontos.py
+++ pontos.py
@@ -1,3 +1,70 @@
-pontosDoacoes = [("Casa do Zézinho", "Espaço de oportunidades de desenvolvimento para crianças e jovens que vivem em situação de alta vulnerabilidade social", "Rua Anália Dolácio Albino, 77 – Parque Maria Helena, SP / CEP 05854-020", "(011) 3939-0257"), ("Lalec", "Casa de Acolhimento para crianças em situação de alta vulnerabilidade e garantimos a elas seu direito a um lar, enquanto aguardam sua reintegração à sociedade", "Av. Indianópolis, 2077 – Planalto Paulista São Paulo – SP – CEP: 04063 – 004"),("Associação prato cheio")]
+pontosDoacoes = {
+    'Comida': {
+        'Zona Sul': [
+            {'nome': 'Casa do Zézinho', 'endereco': 'Rua Anália Dolácio Albino, 77 – Parque Maria Helena, SP', 'telefone': '(11) 3939-0257'},
+            {'nome': 'Largo da Matriz de Nossa Senhora dos Prazeres', 'endereco': 'Centro - Itapecerica - SP - 06850-730', 'telefone': '(11) 4666-3679'}
 
-print(pontosDoacoes)
+        ],
+        'Zona Norte': [
+            {'nome': 'Ponto 3', 'endereco': 'Endereço 3', 'telefone': '333-3333'},
+        ],
+        'Zona Leste': [
+            {'nome': 'Anjos da Noite', 'endereco': ' Endereço Rua Desembargador Rocha Portela, 634 - ARTUR ALVIM - São Paulo - SP', 'telefone': '(11) 98160-8407'},
+        ],
+        'Zona Oeste': [
+            {'nome': 'Associação Prato Cheio', 'endereco': 'Rua Luís Coelho, 308, Consolação - Sala 25 - São Paulo/SP', 'telefone': '(11) 97480-2894'},
+            {'nome': 'Lalec', 'endereco': 'Av. Indianópolis, 2077 – Planalto Paulista São Paulo – SP', 'telefone': '(11) 2275-0713'}
+        ],
+    },
+    'Roupa': {
+       'Zona Sul': [
+            {'nome': 'Casa do Zézinho', 'endereco': 'Rua Anália Dolácio Albino, 77 – Parque Maria Helena, SP', 'telefone': '(11) 3939-0257'},
+            {'nome': 'Brechó Beneficente - APNSP', 'endereco': 'R. Antônio Manoel Pedroso de Castro, 32', 'telefone': '(11) 4667-6367'},
+            {'nome': 'Associação de Apoio à Criança com Câncer (AACC)', 'endereco': 'Rua Borges Lagoa, 1603 – Vila Clementino, São Paulo- SP', 'telefone': '(11) 5084 5434'}
+            
+        ],
+        'Zona Norte': [
+            {'nome': 'Ponto 3', 'endereco': 'Endereço 3', 'telefone': '333-3333'},
+        ],
+        'Zona Leste': [
+            {'nome': 'Unibes Bazar', 'endereco': 'Rua Rodolfo Miranda, 287 – Bom Retiro', 'telefone': '333-3333'},
+        ],
+        'Zona Oeste': [
+            {'nome': 'Lalec', 'endereco': 'Av. Indianópolis, 2077 – Planalto Paulista São Paulo – SP', 'telefone': '(11) 2275-0713'}
+        ],
+    },
+    'Agasalhos': {
+        'Zona Sul': [
+            {'nome': 'Casa do Zézinho', 'endereco': 'Rua Anália Dolácio Albino, 77 – Parque Maria Helena, SP', 'telefone': '(11) 3939-0257'},
+            {'nome': 'Associação de Apoio à Criança com Câncer (AACC)', 'endereco': 'Rua Borges Lagoa, 1603 – Vila Clementino, São Paulo- SP', 'telefone': '(11) 4667-6367'},
+            {'nome': 'Brechó Beneficente - APNSP', 'endereco': 'R. Antônio Manoel Pedroso de Castro, 32', 'telefone': '(11) 4667-6367'},
+        ],
+        'Zona Norte': [
+            {'nome': 'Ponto 3', 'endereco': 'Endereço 3', 'telefone': '333-3333'},
+        ],
+        'Zona Leste': [
+            {'nome': 'Ponto 3', 'endereco': 'Endereço 3', 'telefone': '333-3333'},
+        ],
+        'Zona Oeste': [
+            {'nome': 'Ponto 3', 'endereco': 'Endereço 3', 'telefone': '333-3333'},
+        ],
+    },
+    'Higiene': {
+        'Zona Sul': [
+            {'nome': 'Casa do Zézinho', 'endereco': 'Rua Anália Dolácio Albino, 77 – Parque Maria Helena, SP', 'telefone': '(11) 3939-0257'},
+            {'nome': 'Associação de Apoio à Criança com Câncer (AACC)', 'endereco': 'Rua Borges Lagoa, 1603 – Vila Clementino, São Paulo- SP', 'telefone': '(11) 4667-6367'},
+            {'nome': 'Brechó Beneficente - APNSP', 'endereco': 'R. Antônio Manoel Pedroso de Castro, 32', 'telefone': '(11) 4667-6367'},
+        ],
+        'Zona Norte': [
+            {'nome': 'Ponto 3', 'endereco': 'Endereço 3', 'telefone': '333-3333'},
+        ],
+        'Zona Leste': [
+            {'nome': 'Ponto 3', 'endereco': 'Endereço 3', 'telefone': '333-3333'},
+           
+        ],
+        'Zona Oeste': [
+            {'nome': 'Ponto 3', 'endereco': 'Endereço 3', 'telefone': '333-3333'},
+           
+        ],
+    },
+}

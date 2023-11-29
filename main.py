71b554e4ad68faffe1e88c9b713f8530a2630de9
bruna <brunitabruninha.bfs@gmail.com>
2023-11-29 00:50:35 -0300

    Desenvolvimento
    Implementação de funções e inclusão de mais pontos de coleta e doação.

--- main.py
+++ main.py
@@ -1,9 +1,45 @@
 from pontos import pontosDoacoes
 
+def mostrar_pontos(recurso, regiao):
+    try:
+        pontos_na_regiao = pontosDoacoes[recurso][regiao]
+        print(f"\nPontos de {recurso} na {regiao}:")
+        for ponto in pontos_na_regiao:
+            print(f"{ponto['nome']} \n{ponto['endereco']} \n{ponto['telefone']}\n")
+    except KeyError:
+        print("Recurso ou região não encontrados. Por favor, verifique a entrada.")
+
 def main():
     print("Escolha o recurso que deseja consultar:")
     print("1. Comida")
-    print("2. Roupa")
-    print("3. Agasalho")
-    print("4. Higiene")
+    print("2. Roupas e agasalhos")
+    print("3. Higiene")
     print("4. Doação de sangue")
+
+    opcaoUsuario = int(input("Digite o número do recurso desejado: "))
+
+    if opcaoUsuario < 1 or opcaoUsuario > len(pontosDoacoes):
+        print("Opção inválida. Saindo do programa.")
+        return
+
+    opcao = list(pontosDoacoes.keys())[opcaoUsuario - 1]
+
+    print("\nEscolha a região que deseja consultar:")
+    regioes_disponiveis = list(pontosDoacoes[opcao].keys())
+    for i, regiao in enumerate(regioes_disponiveis, start=1):
+        print(f"{i}. {regiao}")
+
+    regiao_numero = int(input("Digite o número da região desejada: "))
+
+    if regiao_numero < 1 or regiao_numero > len(regioes_disponiveis):
+        print("Opção inválida. Saindo do programa.")
+        return
+
+    regiao_escolhida = regioes_disponiveis[regiao_numero - 1]
+
+    mostrar_pontos(opcao, regiao_escolhida)
+
+if __name__ == "__main__":
+    main()
+
+
--- pontos.py
+++ pontos.py
@@ -2,69 +2,74 @@ pontosDoacoes = {
     'Comida': {
         'Zona Sul': [
             {'nome': 'Casa do Zézinho', 'endereco': 'Rua Anália Dolácio Albino, 77 – Parque Maria Helena, SP', 'telefone': '(11) 3939-0257'},
-            {'nome': 'Largo da Matriz de Nossa Senhora dos Prazeres', 'endereco': 'Centro - Itapecerica - SP - 06850-730', 'telefone': '(11) 4666-3679'}
+            {'nome': 'Largo da Matriz de Nossa Senhora dos Prazeres', 'endereco': 'Centro - Itapecerica - SP - 06850-730', 'telefone': '(11) 4666-3679'},
+            {'nome': 'Centro Cultural São Paulo', 'endereco': 'Centro - Itapecerica - SP - 06850-730', 'telefone': '(11) 3397-4002'},
+            {'nome': 'Centro Cultural da Diversidade', 'endereco': 'Rua Lopes Neto, 206 - Itaim Bibi', 'telefone': '(11) 3079-3438'},
+            {'nome': 'Centro Cultural Santo Amaro', 'endereco': 'Avenida João Dias, 822 - Santo Amaro', 'telefone': '(11) 98397-3660'}
 
         ],
         'Zona Norte': [
-            {'nome': 'Ponto 3', 'endereco': 'Endereço 3', 'telefone': '333-3333'},
+            {'nome': 'Centro de Recreação Infantil Amar Mais Muito Mais', 'endereco': 'Jardim Santa Cruz, São Paulo - SP, 02675-031', 'telefone': '(11) 98894-9209'},
+            {'nome': 'Casa de Cultura Vila Guilherme', 'endereco': 'Praça Oscár da Silva, 110 - Vila Guilherme', 'telefone': '(11) 98397-6169'},
+            {'nome': 'Casa de Doação Davi', 'endereco': 'Rua Antônio Augusto Queiroga, 140 - Vila Serralheiro, São Paulo - SP, 02835-070', 'telefone': '(11) 3852-7330'},
+            {'nome': 'Instituto Alimentar', 'endereco': 'R. Nossa Sra. de Montevergine, 48 - Vila Santana, São Paulo - SP', 'telefone': '(11) 4119-2195'},
+            {'nome': 'Projeto Refeição Solidária', 'endereco': 'Av. Antônio de Souza Noschese, 395 - Parque Continental, São Paulo - SP', 'telefone': '(11) 98824-4634'},
+            {'nome': 'Aquece o Centro', 'endereco': 'Av. Joaquina Ramalho, 156 - Vila Guilherme, São Paulo - SP', 'telefone': '(11) 91317-4880'},
         ],
         'Zona Leste': [
-            {'nome': 'Anjos da Noite', 'endereco': ' Endereço Rua Desembargador Rocha Portela, 634 - ARTUR ALVIM - São Paulo - SP', 'telefone': '(11) 98160-8407'},
+            {'nome': 'Anjos da Noite', 'endereco': 'Endereço Rua Desembargador Rocha Portela, 634 - ARTUR ALVIM - São Paulo - SP', 'telefone': '(11) 98160-8407'},
+            {'nome': 'Teatro Arthur Azevedo', 'endereco': 'Avenida Paes de Barros, 955 – Mooca', 'telefone': '(11) 2604-5558'},
+            {'nome': 'ONG mãos que abençoam', 'endereco': 'Alameda São Caetano, 1160 - Bairro Santa Maria - São Caetano do Sul', 'telefone': '(11) 97999-3230'},
         ],
         'Zona Oeste': [
             {'nome': 'Associação Prato Cheio', 'endereco': 'Rua Luís Coelho, 308, Consolação - Sala 25 - São Paulo/SP', 'telefone': '(11) 97480-2894'},
-            {'nome': 'Lalec', 'endereco': 'Av. Indianópolis, 2077 – Planalto Paulista São Paulo – SP', 'telefone': '(11) 2275-0713'}
+            {'nome': 'Lalec', 'endereco': 'Av. Indianópolis, 2077 – Planalto Paulista São Paulo – SP', 'telefone': '(11) 2275-0713'},
+            {'nome': 'Tendal da Lapa', 'endereco': 'Rua Constança, 72 – Lapa', 'telefone': '(11) 3862-1837'},
+            {'nome': 'Casa de Cultura do Butantã', 'endereco': 'Avenida Junta Mizumoto, 13 - Jardim Peri', 'telefone': '(11) 3742-6218'}
         ],
     },
     'Roupa': {
        'Zona Sul': [
             {'nome': 'Casa do Zézinho', 'endereco': 'Rua Anália Dolácio Albino, 77 – Parque Maria Helena, SP', 'telefone': '(11) 3939-0257'},
             {'nome': 'Brechó Beneficente - APNSP', 'endereco': 'R. Antônio Manoel Pedroso de Castro, 32', 'telefone': '(11) 4667-6367'},
-            {'nome': 'Associação de Apoio à Criança com Câncer (AACC)', 'endereco': 'Rua Borges Lagoa, 1603 – Vila Clementino, São Paulo- SP', 'telefone': '(11) 5084 5434'}
+            {'nome': 'Associação de Apoio à Criança com Câncer', 'endereco': 'Rua Borges Lagoa, 1603 – Vila Clementino, São Paulo- SP', 'telefone': '(11) 5084 5434'}
             
         ],
         'Zona Norte': [
-            {'nome': 'Ponto 3', 'endereco': 'Endereço 3', 'telefone': '333-3333'},
+            {'nome': 'Casa de Doação Davi', 'endereco': 'Rua Antônio Augusto Queiroga, 140 - Vila Serralheiro, São Paulo - SP', 'telefone': '(11) 3852-7330'},
+            {'nome': 'Bazar Unibes', 'endereco': 'R. Voluntários da Pátria, 2286 - Santana, São Paulo - SP', 'telefone': '(11) 2950-3005'},
         ],
         'Zona Leste': [
-            {'nome': 'Unibes Bazar', 'endereco': 'Rua Rodolfo Miranda, 287 – Bom Retiro', 'telefone': '333-3333'},
+            {'nome': 'Unibes Bazar', 'endereco': 'Rua Rodolfo Miranda, 287 – Bom Retiro', 'telefone': '(11) 96929-4660'},
+            {'nome': 'Exército da Salvação Diadema', 'endereco': 'Av. José Bonifácio, 543 543, Serraria Diadema - SP', 'telefone': '(11) 4003-2299'},
+            {'nome': 'Bazar Diadema', 'endereco': 'Rua Manoel da Nóbrega 712, Centro Diadema - SP', 'telefone': '(11) 4003-2299'},
+            {'nome': 'Exército da Salvação Bazar Mauá', 'endereco': 'Rua Luís Mariani 136, Centro Mauá - SP', 'telefone': '(11) 4003-2299'},
         ],
         'Zona Oeste': [
-            {'nome': 'Lalec', 'endereco': 'Av. Indianópolis, 2077 – Planalto Paulista São Paulo – SP', 'telefone': '(11) 2275-0713'}
-        ],
-    },
-    'Agasalhos': {
-        'Zona Sul': [
-            {'nome': 'Casa do Zézinho', 'endereco': 'Rua Anália Dolácio Albino, 77 – Parque Maria Helena, SP', 'telefone': '(11) 3939-0257'},
-            {'nome': 'Associação de Apoio à Criança com Câncer (AACC)', 'endereco': 'Rua Borges Lagoa, 1603 – Vila Clementino, São Paulo- SP', 'telefone': '(11) 4667-6367'},
-            {'nome': 'Brechó Beneficente - APNSP', 'endereco': 'R. Antônio Manoel Pedroso de Castro, 32', 'telefone': '(11) 4667-6367'},
-        ],
-        'Zona Norte': [
-            {'nome': 'Ponto 3', 'endereco': 'Endereço 3', 'telefone': '333-3333'},
-        ],
-        'Zona Leste': [
-            {'nome': 'Ponto 3', 'endereco': 'Endereço 3', 'telefone': '333-3333'},
-        ],
-        'Zona Oeste': [
-            {'nome': 'Ponto 3', 'endereco': 'Endereço 3', 'telefone': '333-3333'},
+            {'nome': 'Paróquia João Bosco', 'endereco': 'Rua Cerro Corá, 2.101 – Alto da Lapa', 'telefone': '(11) 3022-3334'}
         ],
     },
     'Higiene': {
         'Zona Sul': [
             {'nome': 'Casa do Zézinho', 'endereco': 'Rua Anália Dolácio Albino, 77 – Parque Maria Helena, SP', 'telefone': '(11) 3939-0257'},
-            {'nome': 'Associação de Apoio à Criança com Câncer (AACC)', 'endereco': 'Rua Borges Lagoa, 1603 – Vila Clementino, São Paulo- SP', 'telefone': '(11) 4667-6367'},
+            {'nome': 'Associação de Apoio à Criança com Câncer', 'endereco': 'Rua Borges Lagoa, 1603 – Vila Clementino, São Paulo- SP', 'telefone': '11 5084 5434'},
             {'nome': 'Brechó Beneficente - APNSP', 'endereco': 'R. Antônio Manoel Pedroso de Castro, 32', 'telefone': '(11) 4667-6367'},
         ],
         'Zona Norte': [
-            {'nome': 'Ponto 3', 'endereco': 'Endereço 3', 'telefone': '333-3333'},
+            {'nome': 'Casa de Doação Davi', 'endereco': 'Rua Antônio Augusto Queiroga, 140 - Vila Serralheiro, São Paulo - SP', 'telefone': '(11) 3852-7330'},
+            {'nome': 'Bazar Unibes', 'endereco': 'R. Voluntários da Pátria, 2286 - Santana, São Paulo - SP', 'telefone': '(11) 2950-3005'},
         ],
         'Zona Leste': [
-            {'nome': 'Ponto 3', 'endereco': 'Endereço 3', 'telefone': '333-3333'},
+            {'nome': 'Unibes Bazar', 'endereco': 'Rua Rodolfo Miranda, 287 – Bom Retiro', 'telefone': '(11) 96929-4660'},
+            {'nome': 'Bazar Diadema', 'endereco': 'Av. José Bonifácio, 543 543, Serraria Diadema - SP', 'telefone': '(11) 4003-2299'},
+            {'nome': 'Exército da Salvação', 'endereco': 'Rua Manoel da Nóbrega 712, Centro Diadema - SP', 'telefone': '(11) 4003-2299'},
+            {'nome': 'Bazar Mauá', 'endereco': 'Rua Luís Mariani 136, Centro Mauá - SP', 'telefone': '(11) 4003-2299'},
            
         ],
         'Zona Oeste': [
-            {'nome': 'Ponto 3', 'endereco': 'Endereço 3', 'telefone': '333-3333'},
+            {'nome': 'Paróquia João Bosco', 'endereco': 'Rua Cerro Corá, 2.101 – Alto da Lapa', 'telefone': '(11) 3022-3334'}
            
         ],
-    },
+}
+
 }
